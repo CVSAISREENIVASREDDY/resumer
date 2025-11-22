51c77bc2281905abe837651d14ca39c6a2c381cb
CV Sai Sreenivas Reddy <dearsaisreenivas@gmail.com>
2025-11-22 12:16:16 +0530

    v1

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
-import postcss from './postcss.config.js';
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
@@ -11,9 +10,6 @@ export default defineConfig(({ mode }) => {
         host: '0.0.0.0',
       },
       plugins: [react()],
-      css: {
-        postcss
-      },
       resolve: {
         alias: {
           '@': path.resolve(__dirname, '.'),
